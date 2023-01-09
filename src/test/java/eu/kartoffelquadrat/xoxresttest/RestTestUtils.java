d3bc9b86cc401902ac7dcda1f68ecd2195bfa359
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2023-01-09 09:27:09 -0500

    code format

--- src/test/java/eu/kartoffelquadrat/xoxresttest/RestTestUtils.java
+++ src/test/java/eu/kartoffelquadrat/xoxresttest/RestTestUtils.java
@@ -8,36 +8,38 @@ import java.util.Random;
 
 public class RestTestUtils {
 
-    /**
-     * Helper method to tell if CUD was effective by verification by subsequent R is requested in form
-     * of system properties entry. Corresponding maven argument: -Dreadverif=true See:
-     * https://stackoverflow.com/a/9923049
-     */
-    public static boolean isReadVerficationsRequested() {
-        boolean readVerifRequested = Boolean.valueOf(System.getProperty("readverif"));
-        System.out.println("Read verification of corrupting operations enabled: " + readVerifRequested);
-        return readVerifRequested;
-    }
+  /**
+   * Helper method to tell if CUD was effective by verification by subsequent R is requested in form
+   * of system properties entry. Corresponding maven argument: -Dreadverif=true See:
+   * https://stackoverflow.com/a/9923049
+   */
+  public static boolean isReadVerficationsRequested() {
+    boolean readVerifRequested = Boolean.valueOf(System.getProperty("readverif"));
+    System.out.println("Read verification of corrupting operations enabled: " + readVerifRequested);
+    return readVerifRequested;
+  }
 
-    private final String SERVICE_LOCATION = "http://127.0.0.1:8080/xox/";
+  private final String SERVICE_LOCATION = "http://127.0.0.1:8080/xox/";
 
-    /**
-     * Helper method to construct service URI out of provided relative resource location.
-     *
-     * @param localResource as the relative service resource. Must start with leading slash.
-     * @return URI string for the provided local resource.
-     */
-    protected String getServiceURL(String localResource) {
-        return SERVICE_LOCATION + localResource;
-    }
+  /**
+   * Helper method to construct service URI out of provided relative resource location.
+   *
+   * @param localResource as the relative service resource. Must start with leading slash.
+   * @return URI string for the provided local resource.
+   */
+  protected String getServiceURL(String localResource) {
+    return SERVICE_LOCATION + localResource;
+  }
 
-    /**
-     * Helper method to inspect HttpResponse and ensure return code is in 2XX range (OK / other success)
-     * @param response as a previously received HttpResponse object
-     */
-    protected void verifyOk(HttpResponse<String> response) {
+  /**
+   * Helper method to inspect HttpResponse and ensure return code is in 2XX range (OK / other
+   * success)
+   *
+   * @param response as a previously received HttpResponse object
+   */
+  protected void verifyOk(HttpResponse<String> response) {
 
-        int status = response.getStatus();
-        assert status/100 == 2;
-    }
+    int status = response.getStatus();
+    assert status / 100 == 2;
+  }
 }
